f62640db45490d5f079e9810645281452da27b92
Varun Kohli <v.kohli.e@gmail.com>
2012-09-28 17:19:41 -0400

    Stuff

--- HW1/part1/calc/calc.c
+++ HW1/part1/calc/calc.c
@@ -22,28 +22,29 @@ void parseOp(char* inputbuf) {
   char* word;
   char *buf[3];
   int ret;
-	//for loop that splits string by the spaces
+ 
+  //for loop that splits string by the spaces
   for(word = strtok(inputbuf, " ");
       word != NULL;
       word = strtok(NULL, " "))
     {
-		//exit if more than three args
+      //exit if more than three args
       if (i == 3) exit(0);
       buf[i] = word;
       i++;
     }
-	//exit if less than 3 args provided
+  //exit if less than 3 args provided
   if (i < 3) exit(0);
-	
+  
   a = atoi(buf[0]);
   b = atoi(buf[2]);
 
-	//exit if non-numbers passed through
+  //exit if non-numbers passed through
   if ((a == 0 && !strcmp(buf[0],"0")) ||
       (b == 0 && !strcmp(buf[2],"0"))) {
-	 exit(0);
-       }
-	
+    exit(0);
+  }
+  
   if (strcmp(buf[1],"+") == 0) {
     ret = add(a,b);
     printf("%d\n", ret);
@@ -69,8 +70,8 @@ void parseOp(char* inputbuf) {
     printf("%d\n", ret);
     return;
   }
-	/*	exit if non-valid operator passed through
-		or if divide by 0 error                */
+  /*	exit if non-valid operator passed through
+	or if divide by 0 error                */
   else
     exit(0);
 }
---
+++ HW1/part1/hello/hello.c
@@ -0,0 +1,10 @@
+/* vkohli, cgil, rasingh
+   hello.c - prints "Hello world!" to stdout */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Hello world!\n");
+	exit(0);
+}
