41226b66e1454a2e666072302088769239ce2368
Varun Kohli <v.kohli.e@gmail.com>
2012-10-24 17:20:20 -0400

    SWI works, no write, exit is questionable and something is wrong in either the setup or the SWI handler. Crashes at end always

--- HW2/lab2/part2/echo/echo.c
+++ HW2/lab2/part2/echo/echo.c
@@ -1,16 +1,29 @@
 /** @file echo.c
  *
  * @author  Carlos Gil : cgil
- *			Varun Kohli : vkohli
- *			Rishabh Singh : rsingh
+ *	    Varun Kohli : vkohli
+ *	    Rishabh Singh : rasingh
  * @date    10/20/2012
  */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <bits/fileno.h>
+#include <bits/types.h>
 
 int main(int argc, char** argv) {
-
-  write(1, "FUCK THIS SHIT\n", 15);
-  return 0;
+  ssize_t output;
+  int size = 32;
+  char buf[size];
+  //  int i;
+  
+  /*  puts("argc: %d\n", argc);
+  for (i = 0; i < argc; i++) 
+    {
+      printf("argv[%d] = %s\n", i, argv[i]);
+      }*/
+  output = read(STDIN_FILENO, buf, size);
+  // printf(buf);
+  return output;
+  
 }
