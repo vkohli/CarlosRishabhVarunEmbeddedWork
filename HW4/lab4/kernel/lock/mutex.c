7ebd87fb4cfac335b38f545cf128faddb07c52df
rasingh <rishabh@cmu.edu>
2012-12-05 18:17:02 -0500

    added mutex code

--- HW4/lab4/kernel/lock/mutex.c
+++ HW4/lab4/kernel/lock/mutex.c
@@ -25,22 +25,60 @@ mutex_t gtMutex[OS_NUM_MUTEX];
 
 void mutex_init()
 {
+	/*---WRITTEN BY RISHABH---*/
 	
+	/*I'm just assigning 0 to unlock and 1 to lock, and -1 to unassigned. 
+	If it's not this, i'm sure it's a quick fix. TAs told me this is an 
+	easy part in lab.*/
+	for (i = 0; i < OS_NUM_MUTEX; i++)
+	{
+		gtMutex[i] = 0;
+	}
+	
+	return;
 }
 
 int mutex_create(void)
 {
+	/*---WRITTEN BY RISHABH---*/
 	
-	return 1; // fix this to return the correct value
+	/*Look for the smallest index that is still a -1, assign a 0 to it.*/
+	int i = 0;
+	while (i < OS_NUM_MUTEX)
+	{
+		if (gtMutex[i] != -1)
+		{
+			i++;
+		}
+	}
+	
+	gtMutex[i] = 0;
+	return i; 
 }
 
 int mutex_lock(int mutex  __attribute__((unused)))
 {
-	return 1; // fix this to return the correct value
+	/*---WRITTEN BY RISHABH---*/
+	
+	/*Check for Errors*/
+	if (!((mutex >= 0) && (mutex <= OS_NUM_MUTEX))) return EINVAL;
+	if (gtMutex[mutex] == 1) return EDEADLOCK;
+	
+	/*Set that value in the mutex to 1.*/
+	gtMutex[mutex] = 1;
+	return 0; /*Return 0 on success*/
 }
 
 int mutex_unlock(int mutex  __attribute__((unused)))
 {
-	return 1; // fix this to return the correct value
+	/*---WRITTEN BY RISHABH---*/
+	
+	/*Check for Errors*/
+	if (!((mutex >= 0) && (mutex <= OS_NUM_MUTEX))) return EINVAL;
+	if (gtMutex[mutex] == 0) return EDEADLOCK;
+	
+	/*Set that value in the mutex to 1.*/
+	gtMutex[mutex] = 0;
+	return 0; /*Return 0 on success*/
 }
 
